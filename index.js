846acf7121ad8312ec3a652fb872c7c13102cbfe
Melu <melu.franco@gmail.com>
2019-12-09 21:23:00 -0300

    Api endpoint return list of items

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+
+const axios = require('axios');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/items', (req,res) => {
+  const search = req.query.q;
+  axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${search}&limit=4`)
+  .then(function(api_res) {
+
+    const categories = api_res.data.filters[0].values[0].path_from_root.map(category => {
+      return category.name;
+    });
+    const items = api_res.data.results.map((item) => {
+    return {
+      id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: item.price,
+          decimals: 00,
+        },
+        address: item.address.city_name, 
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping,
+      }
+    });
+    const response = {
+      author: {
+        name: 'Melina',
+        lastname: 'Franco',
+      },
+      categories: categories, 
+      items: items,
+    }
+    res.json(response)
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+});
+
+//Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
+app.listen(port);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+console.log('App is listening on port ' + port);
