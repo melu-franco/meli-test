d749d026c2d2e6b69531fafaf13dff9c0e1bb882
Melu <melu.franco@gmail.com>
2019-12-09 21:23:52 -0300

    Fetch list of items on client side

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,38 +1,89 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { colors } from '../styles';
 
+import queryString from 'query-string';
+
 import Header from '../components/Header';
+import ic_shipping from '../assets/ic_shipping.png';
 
 import styled from 'styled-components';
 
 class List extends Component {
+  // Initialize the state
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      categories: [],
+      searchValue: queryString.parse(this.props.location.search).search
+    }
+  }
+
+  // Fetch the list on first mount
+  componentDidMount() {
+    console.log(this.state.searchValue);
+    this.getItems();
+  }
+
+  // Retrieves the list of items from the Express app
+  getItems = () => {
+    fetch(`/api/items?q=${this.state.searchValue}`)
+    .then((res) => {
+      const response = res.json();
+      console.log(response);
+      return response;
+    })
+    .then((data) => {
+      console.log(data);
+      return this.setState({items: data.items, categories: data.categories});
+    })
+    .catch(function(error) {
+      console.log(error);
+    });  
+  }
 
   render() {
+    const { items, categories, searchValue } = this.state;
+
     return (
       <div className="App">
-        <Header />
+        <Header value={searchValue} />
         <div className="container">
           <p className="Breadcrumb">
-              <span></span>
+            {categories.map((category, index) => 
+              <span key={index}>{category}</span>
+            )}
           </p>
+          {/* Check to see if any items are found*/}
+          {items.length ? (
             <ResultsList className="searchResults wrapper">
-                <ResultsItem>
-                    <div className="resultsItem__img"></div>
-                    <div className="resultsItem__info d-flex">
-                      <div className="col-10">
-                        <div className="d-flex resultsItem__info__price">
-                          <p className="price"></p>
+              {/* Render the list of items */}
+              {items.map((item, index) => (
+                  <ResultsItem key={index}>
+                    <Link className="resultsItem d-flex align-items-center" to={`/items/${item.id}`}>
+                        <div className="resultsItem__img" style={{backgroundImage: `url(${item.picture})`}}></div>
+                        <div className="resultsItem__info d-flex">
+                          <div className="col-10">
+                            <div className="d-flex resultsItem__info__price">
+                              <p className="price">{item.price.currency === "USD" ? "U$S" : "$"} {new Intl.NumberFormat("de-DE").format(item.price.amount)}</p>
+                              {item.free_shipping && <img className="free-shipping" src={ic_shipping} alt="Free shipping"/> }
+                            </div>
+                            <p className="resultsItem__info__description">{item.title}</p>
+                          </div>
+                          <p className="resultsItem__info__province col-2">
+                            {item.address}
+                          </p>
                         </div>
-                        <p className="resultsItem__info__description"></p>
-                      </div>
-                      <p className="resultsItem__info__province col-2">
-                        {item.address}
-                      </p>
-                    </div>
-                </ResultsItem>
+                    </Link>
+                  </ResultsItem>
+                ))}
             </ResultsList>
-
+          ) : (
+            <p>Loading..</p>
+          )
+        }
         </div>
       </div>
     );
